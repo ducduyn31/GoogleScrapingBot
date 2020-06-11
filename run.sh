a301908b622b3495bc13d195ad4a943c34aa154b
Daniel Ng <ducduy.ababat@gmail.com>
2020-06-11 02:39:27 -0500

    search for everything in shell code

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 declare -a EXTS=("txt" "pdf" "xml" "odt" "rtf" "ods" "odp" "doc" "docx" "ppt" "pptx" "xls" "xlsx" "html" "csv" "epub" "gz" "zip" "gif" "jar")
 
 for val in ${EXTS[@]}; do
-  python searchndownload.py -i corona -e $val -o $val
+  python searchndownload.py -e $val -o $val
 done
